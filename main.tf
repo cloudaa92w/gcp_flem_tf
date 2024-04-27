c930ea5faf2b195b5581bbde9290692527292407
cloudaa92w <lynn.parrish.952@my.csun.edu>
2024-04-26 23:19:38 -0700

    Modded GCP VPC

--- main.tf
+++ main.tf
@@ -15,26 +15,35 @@ zone = "us-central1-a"
 credentials = "dulcet-eagle-419518-5f8c59a8f5cc.json"
 
 }
-resource "google_storage_bucket" "noscragsbucket" {
+
+resource "google_storage_bucket" "noratchetsbucket" {
   name          = "myversionbucket"
   location      = "US"
   force_destroy = true
 
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
 }
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-sg" {
+  name ="sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.72.1.0/24"
+  region = "us-central1"
+}
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
